655f9f0a175fd8898b950084c9668efaf1c41f85
Varvara Shapko <varya.shepko@gmail.com>
2025-06-02 12:16:40 +0300

    Update sender.cpp

--- src/sender.cpp
+++ src/sender.cpp
@@ -7,83 +7,78 @@ using namespace std;
 int main(int argc, char* argv[]) {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+    
     if (argc < 2) {
-        cout << "Èñïîëüçîâàíèå: sender.exe <èìÿ_ôàéëà>" << endl;
+        cout << "Использование: sender.exe <имя_файла>" << endl;
         return 1;
     }
-
+    
     string fileName = argv[1];
-
+    
     HANDLE hReadyEvent = EventUtils::openNamedEvent(SENDER_READY_EVENT);
     HANDLE hCanWriteEvent = EventUtils::openNamedEvent(CAN_WRITE_EVENT);
     HANDLE hCanReadEvent = EventUtils::openNamedEvent(CAN_READ_EVENT);
-
+    
     if (!hReadyEvent || !hCanWriteEvent || !hCanReadEvent) {
-        cout << "Îøèáêà îòêðûòèÿ ñîáûòèé!" << endl;
+        cout << "Ошибка открытия событий!" << endl;
         return static_cast<int>(ErrorCode::EVENT_OPEN_ERROR);
     }
-
-    cout << "Ïðîöåññ Sender çàïóùåí. Ôàéë: " << fileName << endl;
-
+    
+    cout << "Процесс Sender запущен. Файл: " << fileName << endl;
     EventUtils::signalEvent(hReadyEvent);
-    cout << "Ñèãíàë ãîòîâíîñòè îòïðàâëåí Receiver'ó" << endl;
-
+    cout << "Сигнал готовности отправлен Receiver'у" << endl;
+    
     string command;
     while (true) {
-        cout << "\nÂâåäèòå êîìàíäó (send - îòïðàâèòü ñîîáùåíèå, exit - âûõîä): ";
+        cout << "\nВведите команду (send - отправить сообщение, exit - выход): ";
         cin >> command;
-
+        
         if (command == "exit") {
             break;
         }
         else if (command == "send") {
             EventUtils::waitForEvent(hCanWriteEvent);
-
+            
             string messageText;
-            cout << "Ââåäèòå òåêñò ñîîáùåíèÿ (ìàêñ. 19 ñèìâîëîâ): ";
+            cout << "Введите текст сообщения (макс. 19 символов): ";
             cin.ignore(); 
             getline(cin, messageText);
-
+            
             if (messageText.length() >= MAX_MESSAGE_LENGTH) {
                 messageText = messageText.substr(0, MAX_MESSAGE_LENGTH - 1);
-                cout << "Ñîîáùåíèå îáðåçàíî äî: " << messageText << endl;
+                cout << "Сообщение обрезано до: " << messageText << endl;
             }
-
+            
             HANDLE hFile = FileUtils::openMessageFile(fileName.c_str());
-
             if (hFile != INVALID_HANDLE_VALUE) {
                 Message msg;
-
                 if (FileUtils::readMessage(hFile, msg)) {
                     if (msg.isEmpty) {
                         msg.setText(messageText.c_str());
                         FileUtils::writeMessage(hFile, msg);
-
-                        cout << "Ñîîáùåíèå îòïðàâëåíî: " << messageText << endl;
-
+                        cout << "Сообщение отправлено: " << messageText << endl;
                         EventUtils::resetEvent(hCanWriteEvent);
                         EventUtils::signalEvent(hCanReadEvent);
                     }
                     else {
-                        cout << "Ôàéë çàíÿò, îæèäàíèå îñâîáîæäåíèÿ..." << endl;
+                        cout << "Файл занят, ожидание освобождения..." << endl;
                     }
                 }
                 CloseHandle(hFile);
             }
             else {
-                cout << "Îøèáêà îòêðûòèÿ ôàéëà!" << endl;
+                cout << "Ошибка открытия файла!" << endl;
             }
         }
         else {
-            cout << "Íåèçâåñòíàÿ êîìàíäà!" << endl;
+            cout << "Неизвестная команда!" << endl;
         }
     }
-
+    
     CloseHandle(hReadyEvent);
     CloseHandle(hCanWriteEvent);
     CloseHandle(hCanReadEvent);
-
-    cout << "Ïðîöåññ Sender çàâåðøåí." << endl;
+    cout << "Процесс Sender завершен." << endl;
+    
     return 0;
 }
